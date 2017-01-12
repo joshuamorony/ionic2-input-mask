42e108928a887420a6636ec7ba7ea2c1e1beee53
Joshua Morony <joshuamorony@Joshuas-MacBook-Pro.local>
2017-01-12 17:01:38 +1030

    initial commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+	masks: any;
+
+	phoneNumber: any = "";
+	cardNumber: any = "";
+	cardExpiry: any = "";
+	orderCode: any = "";
+
+	constructor(public navCtrl: NavController) {
+
+		this.masks = {
+			phoneNumber: ['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/],
+			cardNumber: [/\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/],
+			cardExpiry: [/[0-1]/, /\d/, '/', /[1-2]/, /\d/],
+			orderCode: [/[a-zA-z]/, ':', /\d/, /\d/, /\d/, /\d/]
+		};
+
+	}
+
+	save(){
+
+		let unmaskedData = {
+			phoneNumber: this.phoneNumber.replace(/\D+/g, ''),
+			cardNumber: this.cardNumber.replace(/\D+/g, ''),
+			cardExpiry: this.cardExpiry.replace(/\D+/g, ''),
+			orderCode: this.orderCode.replace(/[^a-zA-Z0-9 -]/g, '')
+		};
+
+		console.log(unmaskedData);
+
+	}
+
+}
